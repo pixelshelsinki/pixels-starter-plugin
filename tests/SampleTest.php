9bedbb31b5c5d773fd9fc76819ac731e13f62786
stscoundrel <sampo@pixels.fi>
2020-10-06 10:52:44 +0300

    Add PHPUnit & sample test

---
+++ tests/SampleTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Sample class for unit tests.
+ *
+ * @package ProjectName.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Sample class unit tests.
+ */
+final class SampleTest extends TestCase {
+
+	/**
+	 * Verify tests run.
+	 */
+	public function testCanRunTests() {
+
+		$this->assertTrue(
+			true
+		);
+
+		$this->assertFalse(
+			false
+		);
+	}
+
+}
