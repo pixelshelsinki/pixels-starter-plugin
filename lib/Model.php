3c8224afbc70ddaea2ed1933e2ec6656c1b7a8d3
stscoundrel <sampo@pixels.fi>
2021-01-26 17:20:02 +0200

    Model: run cpt & tax instances through setter.
    Forces instances to have set Interfaces even in pre 7.4 environments through type hint in function.

--- lib/Model.php
+++ lib/Model.php
@@ -7,6 +7,10 @@
 
 namespace Pixels\ProjectName;
 
+// Contracts.
+use Pixels\ProjectName\Model\PostTypes\Contracts\PostTypeInterface;
+use Pixels\ProjectName\Model\Taxonomies\Contracts\TaxonomyInterface;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
@@ -22,21 +26,19 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Model {
 
-	// Class properties.
+	/**
+	 * Post types.
+	 *
+	 * @var array.
+	 */
+	private $post_types = array();
 
 	/**
-	 * Example Post Type.
+	 * Taxonomies
 	 *
-	 * @var Example
+	 * @var array.
 	 */
-	private $cpt_example;
-
-	/**
-	 * Example Taxonomy
-	 *
-	 * @var ExampleTaxonomy
-	 */
-	private $tax_example;
+	private $taxonomies = array();
 
 	/**
 	 * Meta fields.
@@ -65,10 +67,10 @@ class Model {
 	public function __construct() {
 
 		// Custom post types.
-		$this->cpt_example = new Model\PostTypes\Example();
+		$this->add_post_type( 'example', new Model\PostTypes\Example() );
 
 		// Taxonomies.
-		$this->tax_example = new Model\Taxonomies\ExampleTaxonomy();
+		$this->add_taxonomy(  'example_taxonomy', new Model\Taxonomies\ExampleTaxonomy() );
 
 		// Fields.
 		$this->meta_example = new Model\Meta\Fields\Example();
@@ -76,7 +78,26 @@ class Model {
 		// Misc.
 		$this->options_pages = new Model\OptionsPages();
 		$this->acf           = new Model\Meta\ACF();
+	}
 
+	/**
+	 * Add Post Type.
+	 *
+	 * @param string $name
+	 * @param PostTypeInterface $post_type
+	 */
+	public function add_post_type( string $name, PostTypeInterface $post_type ) {
+		$this->post_types[ $name ] = $post_type;
+	}
+
+	/**
+	 * Add Post Type.
+	 *
+	 * @param string $name
+	 * @param TaxonomyInterface $post_type
+	 */
+	public function add_taxonomy( string $name, TaxonomyInterface $taxonomy ) {
+		$this->taxonomies[ $name ] = $taxonomy;
 	}
 
 }
